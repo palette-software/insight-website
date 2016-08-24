004b8f0196717e14f14dc99d16977d31a1a2a728
Szilard Huber <szilard.huber@gmail.com>
2016-08-24 11:55:50 +0200

    Display status as JSON for now.

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ import SocketServer
 import os
 import socket
 import subprocess
+import json
+import re
 
 PORT = 9080
 
@@ -37,6 +39,87 @@ def handler_fn(handler_fn, content_type="text/plain"):
         req.wfile.write(response)
 
     return render
+
+def get_dummy_subprocess_output(command_with_args):
+    with open('sample.txt', 'r') as myfile:
+        data = myfile.read()
+        return data
+
+
+def status_handler(command_with_args):
+    """ Creates a handler that responds with the output of running the command
+    throught POPEN """
+    def handler(req):
+        if not os.environ.has_key('DEBUG'):
+            try:
+                status = subprocess.check_output(command_with_args, stderr=subprocess.STDOUT)
+            except Exception:
+                status = "{}"
+        else:
+            status = get_dummy_subprocess_output(command_with_args)
+        status_json = parse_status(status)
+        return status_json
+
+    return handler_fn(handler)
+
+
+def parse_status(status):
+    data = {}
+    data["greenplum"] = {}
+    match = re.search("local Greenplum Version: 'postgres \(Greenplum Database\) (.+?)'", status)
+    if match:
+        data['greenplum']['local-greenplum-version'] = match.group(1)
+
+    match = re.search("master Greenplum Version: '(.+?\s.+?) .*'", status)
+    if match:
+        data['greenplum']['master-greenplum-version'] = match.group(1)
+
+    match = re.search("Master instance\s+=\s(.+)", status)
+    if match:
+        data['greenplum']['active'] = match.group(1) == 'Active'
+    else:
+        data['greenplum']['active'] = False
+
+    match = re.search("Total segment instance count from metadata.*(\d+)", status)
+    if match:
+        data['greenplum']['segments-from-metadata'] = match.group(1)
+
+    match = re.search("Total primary segments.*(\d+)", status)
+    if match:
+        data['greenplum']['total-primary-segments'] = match.group(1)
+
+    match = re.search("Total primary segment valid \(at master\).*(\d+)", status)
+    if match:
+        data['greenplum']['valid-primary-segments'] = match.group(1)
+
+    match = re.search("Total primary segment failures \(at master\).*(\d+)", status)
+    if match:
+        data['greenplum']['primary-segment-failures'] = match.group(1)
+
+    match = re.search("insight-gpfdist\s+(\w+)\s+pid\s(\d+), uptime (.+?), \d+:\d+:\d+", status)
+    data['gpfdist'] = {}
+    if match:
+        data['gpfdist']['active'] = match.group(1) == 'RUNNING'
+        data['gpfdist']['pid'] = match.group(2)
+        data['gpfdist']['uptime'] = match.group(3)
+    else:
+        data['gpfdist']['active'] = False
+
+    data['datamodel'] = {}
+    match = re.search("(.*?([^\s]+\s){5}/opt/palette-insight-talend/load_tables.sh)", status)
+    if match:
+        data['datamodel']['load_tables'] = match and not re.search("\#", match.group(1))
+    else:
+        data['datamodel']['load_tables'] = False
+
+    match = re.search("(.*?([^\s]+\s){5}/home/insight/loadctrl.sh)", status)
+    if match:
+        data['datamodel']['reporting'] = match and not re.search("\#", match.group(1))
+    else:
+        data['datamodel']['reporting'] = False
+
+    # return status
+    return json.dumps(data, indent=4, sort_keys=True)
 
 
 def command_handler(command_with_args):
@@ -85,10 +168,7 @@ class HTTPServer(SocketServer.TCPServer):
 HANDLER_MAP = {
         '/': page('index.jade'),
         '/control': page('control.jade'),
-        '/start': command_handler(["sudo", "/usr/local/bin/insight-services", "start"]),
-        '/stop' : command_handler(["sudo", "/usr/local/bin/insight-services", "stop"]),
-        '/status' : command_handler(["sudo", "/usr/local/bin/insight-services", "status"]),
-        # '/status' : command_handler(["sudo", "ls", "-ltr"]),
+        '/status' : status_handler(["sudo", "/usr/local/bin/insight-services", "status"]),
         }
 # ==================== Handlers ====================
 
